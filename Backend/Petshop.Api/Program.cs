37a9611f269af7131e711f7019774d61e3c90c0a
Higoralmeida702 <higoralmeida7022@gmail.com>
2025-02-01 18:49:05 -0300

    feat: configura serviços de autenticação e Swagger no projeto
    -Adiciona SenhaService, UsuarioAuthService e UsuarioRepository como serviços no contêiner de dependências.
    -Configura Swagger para documentação da API.

--- Backend/Petshop.Api/Program.cs
+++ Backend/Petshop.Api/Program.cs
@@ -1,15 +1,41 @@
 using Microsoft.EntityFrameworkCore;
+using Petshop.Application.Interfaces.Auth;
+using Petshop.Application.Services.Auth;
+using Petshop.Domain.Interfaces;
 using Petshop.Infra.Data.Data;
+using Petshop.Infra.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-
 var connectionString = builder.Configuration.GetConnectionString("Database");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddControllers();
+
+
+builder.Services.AddScoped<ISenhaService, SenhaService>();
+builder.Services.AddScoped<IUsuarioAuthService, UsuarioAuthService>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Petshop API V1");
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
