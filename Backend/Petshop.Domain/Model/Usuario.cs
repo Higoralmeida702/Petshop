e2bb6ca6e464dc1f91d2e16c11a2b11d9c23ebfd
Higoralmeida702 <higoralmeida7022@gmail.com>
2025-02-04 22:22:45 -0300

    feat: adicionar validações e coleção de Animais na classe Usuario
    -Adicionada a coleção Animais à classe Usuario, representando a relação de um usuário com múltiplos animais.
    -Implementadas validações nas propriedades da classe Usuario

--- Backend/Petshop.Domain/Model/Usuario.cs
+++ Backend/Petshop.Domain/Model/Usuario.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Petshop.Domain.Validations;
 
 namespace Petshop.Domain.Model
 {
@@ -38,6 +40,8 @@ namespace Petshop.Domain.Model
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
 
+        public ICollection<Animal> Animais { get; set; } = new List<Animal>();
+
         public Usuario(string nome, string sobrenome, string numeroTelefone, string endereco, string email)
         {
             ValidateDomain(nome, sobrenome, numeroTelefone, endereco, email);
@@ -45,6 +49,16 @@ namespace Petshop.Domain.Model
 
         public void ValidateDomain(string nome, string sobrenome, string numeroTelefone, string endereco, string email)
         {
+
+            DomainExceptionValidations.When(nome.Length < 3 || nome.Length > 30, "O nome deve ter entre 3 e 30 caracteres");
+            DomainExceptionValidations.When(sobrenome.Length < 3 || sobrenome.Length > 50, "O sobrenome deve ter entre 3 e 50 caracteres");
+            DomainExceptionValidations.When(numeroTelefone.Length != 11, "O telefone deve ter 11 caracteres");
+            DomainExceptionValidations.When(endereco.Length < 3 || endereco.Length > 100, "A localização deve ter entre 3 e 100 caracteres");
+            DomainExceptionValidations.When(email.Length < 15 || email.Length > 80, "O email deve ter entre 15 e 80 caracteres");
+
+            DomainExceptionValidations.When(!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"), "O email fornecido é inválido");
+            DomainExceptionValidations.When(!Regex.IsMatch(numeroTelefone, @"^\d+$"), "O número de telefone deve conter apenas números.");
+
             Nome = nome;
             Sobrenome = sobrenome;
             NumeroTelefone = numeroTelefone;
