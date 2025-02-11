2c947baeb702f2c41e9245698b8e97e30926c4f1
Higoralmeida702 <higoralmeida7022@gmail.com>
2025-02-11 00:05:39 -0300

    feat: Adicionado endpoints para atualizar e obter perfil
    -Implementados endpoints PUT /atualizar-perfil e GET /obter-perfil
    -Utilizado [Authorize] para proteger as rotas

--- Backend/Petshop.Api/Controller/UsuarioController.cs
+++ Backend/Petshop.Api/Controller/UsuarioController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Petshop.Application.Dto;
 using Petshop.Application.Interfaces.Auth;
 using Petshop.Application.Common.Responses;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 namespace Petshop.Api.Controller
 {
     [ApiController]
@@ -37,6 +39,41 @@ namespace Petshop.Api.Controller
             {
                 return BadRequest(resposta);
             }
+            return Ok(resposta);
+        }
+
+
+        [Authorize]
+        [HttpPut("atualizar-perfil")]
+        public async Task<IActionResult> AtualizarPerfil([FromBody] AtualizarInfoUsuarioDto infoUsuarioDto)
+        {
+            var usuarioIdClaim = User.FindFirst("id")?.Value;
+            if (string.IsNullOrEmpty(usuarioIdClaim))
+                return Unauthorized(new { Mensagem = "Usuário não autenticado!" });
+
+            int usuarioId = int.Parse(usuarioIdClaim);
+            var resposta = await _usuarioAuthService.AtualizarPerfil(usuarioId, infoUsuarioDto);
+
+            if (!resposta.Status)
+                return BadRequest(resposta);
+
+            return Ok(resposta);
+        }
+
+        [Authorize]
+        [HttpGet("obter-perfil")]
+        public async Task<IActionResult> ObterPerfil()
+        {
+            var usuarioIdClaim = User.FindFirst("id")?.Value;
+            if (string.IsNullOrEmpty(usuarioIdClaim))
+                return Unauthorized(new { Mensagem = "Usuário não autenticado!" });
+
+            int usuarioId = int.Parse(usuarioIdClaim);
+            var resposta = await _usuarioAuthService.ObterPerfil(usuarioId);
+
+            if (!resposta.Status)
+                return BadRequest(resposta);
+
             return Ok(resposta);
         }
     }
